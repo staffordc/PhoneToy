c6acd44ef18d776199702a706de12dc351350672
Unknown <stafford.carh@gmail.com>
2019-10-22 10:24:02 -0400

    Horse when clicked noises 5 sec
    There is now a variable that's just for if a "play button" is hit. On first non-recorded play through it makes noises semi-randomly

--- PhoneToy/Assets/Scripts/HorseButtonBehaviors.cs
+++ PhoneToy/Assets/Scripts/HorseButtonBehaviors.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class HorseButtonBehaviors : MonoBehaviour
 {
-    [SerializeField] float screenWidthInUnits = 16f;
-    [SerializeField] float screenHeightInUnits = 16f;
-    [SerializeField] float minX = 6f;
-    [SerializeField] float maxX = 10f;
-    [SerializeField] float maxY = 10f;
-    [SerializeField] float minY = 6f;
     bool horseClicked = false;
     AudioSource myAudioSource;
-
+    IEnumerator CurrentSequence = null;
 
     [SerializeField] AudioClip[] horseSounds;
 
@@ -27,22 +20,55 @@ public class HorseButtonBehaviors : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            HorsePressed();
-        }
+            Debug.Log("Got to the Get MouseButtonDown" + gameObject.name);
 
-    }
 
-    private void HorsePressed()
-    {
-        float[] timings = new float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
-        if (Input.GetMouseButtonDown(0))
-        {
+            Debug.Log("Got to the check for null" + gameObject.name);
             horseClicked = true;
-            AudioClip clip = horseSounds[UnityEngine.Random.Range(0, horseSounds.Length)];
-            myAudioSource.PlayOneShot(clip);
-
+            Debug.Log("HorseClicked is true now");
+            CurrentSequence = DoSequence();
+            Debug.Log("DoSequence is assigned to Current Sequence");
+            StartCoroutine(CurrentSequence);
+            Debug.Log("CurrentSequence was run");
         }
 
     }
 
+    // private void HorsePressed()
+    // {
+    //     float[] timings = new float[] { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         horseClicked = true;
+    //         AudioClip clip = horseSounds[UnityEngine.Random.Range(0, horseSounds.Length)];
+    //         myAudioSource.PlayOneShot(clip);
+
+    //     }
+
+    // }
+
+    IEnumerator DoSequence()
+    {
+        //randomFrameHit is what it is on first play, Recording next
+        var randomFrameHit = 1.0f;
+        var loopRan = 0f;
+        var timeElapsed = 0f;
+        while (timeElapsed <= 5f)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > loopRan)
+            {
+                AudioClip clip = horseSounds[UnityEngine.Random.Range(0, horseSounds.Length)];
+                myAudioSource.PlayOneShot(clip);
+                randomFrameHit = UnityEngine.Random.Range(0f, 1.5f);
+                loopRan += randomFrameHit;
+                Debug.Log("active thing you're watching " + loopRan);
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        CurrentSequence = null;
+        horseClicked = false;
+    }
 }
